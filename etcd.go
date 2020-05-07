da3d66675c1442601eabd9ebd00728a1e9bb174e
dxvgef <dxvgef@outlook.com>
2020-05-07 15:06:17 +0800

    增加etcd的参数

--- etcd.go
+++ etcd.go
@@ -4,12 +4,21 @@ import (
 	"go.etcd.io/etcd/clientv3"
 )
 
-var EtcdClient *clientv3.Client
+var etcdCli *clientv3.Client
 
-func setEtcdClient() (err error) {
-	EtcdClient, err = clientv3.New(clientv3.Config{
-		Endpoints:   []string{"192.168.50.212:2379"},
-		DialTimeout: LocalConfig.ETCD.DialTimeout,
+func setEtcdCli() (err error) {
+	etcdCli, err = clientv3.New(clientv3.Config{
+		Endpoints:            localConfig.ETCD.Endpoints,
+		DialTimeout:          localConfig.ETCD.DialTimeout,
+		Username:             localConfig.ETCD.Username,
+		Password:             localConfig.ETCD.Password,
+		AutoSyncInterval:     localConfig.ETCD.AutoSyncInterval,
+		DialKeepAliveTime:    localConfig.ETCD.DialKeepAliveTime,
+		DialKeepAliveTimeout: localConfig.ETCD.DialKeepAliveTimeout,
+		MaxCallSendMsgSize:   localConfig.ETCD.MaxCallSendMsgSize,
+		MaxCallRecvMsgSize:   localConfig.ETCD.MaxCallRecvMsgSize,
+		RejectOldCluster:     localConfig.ETCD.RejectOldCluster,
+		PermitWithoutStream:  localConfig.ETCD.PermitWithoutStream,
 	})
 	return
 }
