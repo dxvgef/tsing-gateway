5c0064262514c72c9f8c50b6e202badb0484c739
dxvgef <dxvgef@outlook.com>
2020-05-20 13:11:03 +0800

    加入ParseRoute()方法并修复bug

--- global/util.go
+++ global/util.go
@@ -1,8 +1,12 @@
 package global
 
 import (
+	"errors"
+	"path"
 	"strings"
 	"unsafe"
+
+	"github.com/dxvgef/gommon/slice"
 )
 
 var Methods = []string{
@@ -32,4 +36,45 @@ func FormatTime(str string) string {
 	str = strings.Replace(str, "i", "04", -1)
 	str = strings.Replace(str, "s", "05", -1)
 	return str
+}
+
+func ParseRoute(key, keyPrefix string) (routeGroupID, routePath, routeMethod string, err error) {
+	keyPrefix += "/routes/"
+
+	// 确定第一次要裁剪的位置，为了去掉前缀
+	pos := strings.Index(key, keyPrefix)
+	if pos == -1 {
+		err = errors.New("键名前缀处理失败")
+		return
+	}
+	key = key[pos+len(keyPrefix):]
+
+	// 确定第二次要裁剪的位置，为了获得路由组ID
+	pos = strings.Index(key, "/")
+	if pos == -1 {
+		err = errors.New("路由解析失败")
+		return
+	}
+	routeGroupID = key[:pos]
+	if routeGroupID == "" {
+		err = errors.New("路由组ID解析失败")
+		return
+	}
+	key = key[pos:]
+
+	// 获取方法(最后一个路径)
+	routeMethod = path.Base(key)
+	if !slice.InStr(Methods, routeMethod) {
+		err = errors.New("路由方法解析失败")
+		return
+	}
+
+	// 确定第三次要裁剪的位置，为了去掉方法
+	pos = strings.LastIndex(key, "/"+routeMethod)
+	if pos == -1 {
+		err = errors.New("获取方法的位置失败")
+		return
+	}
+	routePath = key[:pos]
+	return
 }
