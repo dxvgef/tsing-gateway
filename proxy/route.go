97a61257ca65cfd133ba5c0ac6618253bee85389
dxvgef <dxvgef@outlook.com>
2020-05-20 13:16:38 +0800

    优化代码

--- proxy/route.go
+++ proxy/route.go
@@ -12,87 +12,58 @@ import (
 )
 
 // 新建路由组及路由
-func (p *Engine) NewRoute(routeGroupID, reqPath, reqMethod, upstreamID string) error {
-	if routeGroupID == "" {
-		routeGroupID = global.GetIDStr()
+func (p *Engine) NewRoute(routeGroupID, routePath, routeMethod, upstreamID string) error {
+	if _, exist := p.Routes[routeGroupID][routePath][routeMethod]; exist {
+		return errors.New("路由组ID:" + routeGroupID + "/路径:" + routePath + "/方法:" + routeMethod + "已存在")
 	}
-	if routeGroupID == "" {
-		return errors.New("没有传入路由组ID,并且无法自动创建ID")
-	}
-	if reqPath == "" {
-		reqPath = "/"
-	}
-	if reqMethod == "" {
-		reqMethod = "*"
-	} else {
-		reqMethod = strings.ToUpper(reqMethod)
-	}
-	if _, exist := p.Routes[routeGroupID]; exist {
-		return errors.New("路由组ID:" + routeGroupID + "已存在")
-	}
-	if _, exist := p.Routes[routeGroupID][reqPath]; exist {
-		return errors.New("路由组ID:" + routeGroupID + "的路径:" + reqPath + "已存在")
-	}
-	if _, exist := p.Routes[routeGroupID][reqPath][reqMethod]; exist {
-		return errors.New("路由组ID:" + routeGroupID + "/路径:" + reqPath + "/方法:" + reqMethod + "已存在")
-	}
-	// if _, exist := p.Upstreams[upstreamID]; !exist {
-	// 	return errors.New("上游ID:" + upstreamID + "不存在")
-	// }
-	p.Routes[routeGroupID] = make(map[string]map[string]string)
-	p.Routes[routeGroupID][reqPath] = make(map[string]string)
-	p.Routes[routeGroupID][reqPath][reqMethod] = upstreamID
-	return nil
+	return p.SetRoute(routeGroupID, routePath, routeMethod, upstreamID)
 }
 
 // 设置路由组及路由，如果存在则更新，不存在则新建
-func (p *Engine) SetRoute(routeGroupID, reqPath, reqMethod, upstreamID string) error {
-	if routeGroupID == "" {
-		routeGroupID = global.GetIDStr()
-	}
+func (p *Engine) SetRoute(routeGroupID, routePath, routeMethod, upstreamID string) error {
 	if routeGroupID == "" {
 		return errors.New("没有传入路由组ID,并且无法自动创建ID")
 	}
 	// if _, exist := p.Upstreams[upstreamID]; !exist {
 	// 	return errors.New("上游ID:" + upstreamID + "不存在")
 	// }
-	if reqPath == "" {
-		reqPath = "/"
+	if routePath == "" {
+		routePath = "/"
 	}
-	if reqMethod == "" {
-		reqMethod = "*"
+	if routeMethod == "" {
+		routeMethod = "*"
 	} else {
-		reqMethod = strings.ToUpper(reqMethod)
+		routeMethod = strings.ToUpper(routeMethod)
 	}
-	if !slice.InStr(global.Methods, reqMethod) {
+	if !slice.InStr(global.Methods, routeMethod) {
 		return errors.New("HTTP方法无效")
 	}
 	if _, exist := p.Routes[routeGroupID]; !exist {
 		p.Routes[routeGroupID] = make(map[string]map[string]string)
 	}
-	if _, exist := p.Routes[routeGroupID][reqPath]; !exist {
-		p.Routes[routeGroupID][reqPath] = make(map[string]string)
+	if _, exist := p.Routes[routeGroupID][routePath]; !exist {
+		p.Routes[routeGroupID][routePath] = make(map[string]string)
 	}
-	p.Routes[routeGroupID][reqPath][reqMethod] = upstreamID
+	p.Routes[routeGroupID][routePath][routeMethod] = upstreamID
 	return nil
 }
 
 // 删除路由
-func (p *Engine) DelRoute(routeGroupID, reqPath, reqMethod string) error {
+func (p *Engine) DelRoute(routeGroupID, routePath, routeMethod string) error {
 	if routeGroupID == "" {
 		routeGroupID = global.GetIDStr()
 	}
 	if routeGroupID == "" {
 		return errors.New("routeGroupID不能为空")
 	}
-	if reqPath == "" {
+	if routePath == "" {
 		return errors.New("reqPath不能为空")
 	}
-	if reqMethod == "" {
+	if routeMethod == "" {
 		return errors.New("reqMethod不能为空")
 	}
-	reqMethod = strings.ToUpper(reqMethod)
-	delete(p.Routes[routeGroupID][reqPath], reqMethod)
+	routeMethod = strings.ToUpper(routeMethod)
+	delete(p.Routes[routeGroupID][routePath], routeMethod)
 	return nil
 }
 
@@ -133,35 +104,36 @@ func (p *Engine) MatchRoute(req *http.Request) (upstream Upstream, status int) {
 }
 
 // 匹配路径，返回最终匹配到的路径
-func (p *Engine) MatchPath(routeGroupID, reqPath string) (string, bool) {
-	if reqPath == "" {
-		reqPath = "/"
+func (p *Engine) MatchPath(routeGroupID, reoutePath string) (string, bool) {
+	if reoutePath == "" {
+		reoutePath = "/"
 	}
 	// 先尝试完全匹配路径
-	if _, exist := p.Routes[routeGroupID][reqPath]; exist {
-		return reqPath, true
+	if _, exist := p.Routes[routeGroupID][reoutePath]; exist {
+		return reoutePath, true
 	}
 	// 尝试模糊匹配
-	pathLastChar := reqPath[len(reqPath)-1]
+	pathLastChar := reoutePath[len(reoutePath)-1]
 	if pathLastChar != 47 {
-		pos := strings.LastIndex(reqPath, path.Base(reqPath))
-		reqPath = reqPath[:pos]
+		pos := strings.LastIndex(reoutePath, path.Base(reoutePath))
+		reoutePath = reoutePath[:pos]
 	}
-	reqPath = reqPath + "*"
-	if _, exist := p.Routes[routeGroupID][reqPath]; exist {
-		return reqPath, true
+	// todo 可能要将*号换成别的符号，因为和api(tsing)的路由规则冲突
+	reoutePath = reoutePath + "*"
+	if _, exist := p.Routes[routeGroupID][reoutePath]; exist {
+		return reoutePath, true
 	}
-	return reqPath, false
+	return reoutePath, false
 }
 
 // 匹配方法，返回对应的集群ID
-func (p *Engine) MatchMethod(routeGroupID, reqPath, reqMethod string) (string, bool) {
-	if _, exist := p.Routes[routeGroupID][reqPath][reqMethod]; exist {
-		return reqMethod, true
+func (p *Engine) MatchMethod(routeGroupID, routePath, routeMethod string) (string, bool) {
+	if _, exist := p.Routes[routeGroupID][routePath][routeMethod]; exist {
+		return routeMethod, true
 	}
-	reqMethod = "*"
-	if _, exist := p.Routes[routeGroupID][reqPath][reqMethod]; exist {
-		return reqMethod, true
+	routeMethod = "*"
+	if _, exist := p.Routes[routeGroupID][routePath][routeMethod]; exist {
+		return routeMethod, true
 	}
-	return reqMethod, false
+	return routeMethod, false
 }
