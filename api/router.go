1a2477f2ae0f9e3450ec9705021c79bb61351e10
dxvgef <dxvgef@outlook.com>
2020-05-21 11:03:52 +0800

    更新注释

--- api/router.go
+++ api/router.go
@@ -4,7 +4,7 @@ import "github.com/dxvgef/tsing"
 
 // 设置路由
 func SetRouter(engine *tsing.Engine) {
-	// 检查secert
+	// 检查secret
 	router := engine.Group("", checkSecretFromHeader)
 
 	var proxyHandler Proxy
