71fe49456aacd78dcb6191e430a70548fe13a5a9
dxvgef <dxvgef@outlook.com>
2020-05-21 10:25:05 +0800

    移除GetIDStr和GetIDInt函数

--- global/util.go
+++ global/util.go
@@ -4,36 +4,8 @@ import (
 	"errors"
 	"path"
 	"strings"
-	"time"
 	"unsafe"
-
-	"github.com/bwmarrin/snowflake"
-	"github.com/rs/zerolog/log"
 )
-
-// 获得ID string值
-func GetIDStr() string {
-	var err error
-	snowflake.Epoch = time.Now().Unix()
-	IDNode, err = snowflake.NewNode(0)
-	if err != nil {
-		log.Error().Msg(err.Error())
-		return ""
-	}
-	return IDNode.Generate().String()
-}
-
-// 获得ID int64值
-func GetIDInt64() int64 {
-	var err error
-	snowflake.Epoch = time.Now().Unix()
-	IDNode, err = snowflake.NewNode(0)
-	if err != nil {
-		log.Error().Msg(err.Error())
-		return 0
-	}
-	return IDNode.Generate().Int64()
-}
 
 func BytesToStr(value []byte) string {
 	return *(*string)(unsafe.Pointer(&value)) // nolint
