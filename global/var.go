b863cd70c82041907fb586347481c3afe1f02286
dxvgef <dxvgef@outlook.com>
2020-05-24 16:39:51 +0800

    clean

--- global/var.go
+++ global/var.go
@@ -17,11 +17,11 @@ var (
 	Routes             = make(map[string]map[string]map[string]string) // 路由列表
 	Upstreams          = make(map[string]UpstreamType)                 // 上游列表
 	UpstreamMiddleware = make(map[string][]MiddlewareType)             // 所有上游的中间件实例
-	Endpoints          = make(map[string][]EndpointType)               // 所有端点列表
+	// Endpoints          = make(map[string][]EndpointType)               // 所有端点列表
 	// HTTP方法允许的值
 	Methods = []string{
 		"ANY", "GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS", "TRACE", "CONNECT",
 	}
 	// 负载均衡算数允许的值
-	LoadBalance = []string{"discover", "wred"}
+	// LoadBalance = []string{"discover", "wred"}
 )
