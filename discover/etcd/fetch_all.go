d793a76ce2ef065477c39f036d0cb848bf09638a
dxvgef <dxvgef@outlook.com>
2020-05-27 20:17:47 +0800

    clean

--- discover/etcd/fetch_all.go
+++
@@ -1,8 +0,0 @@
-package etcd
-
-import "github.com/dxvgef/tsing-gateway/global"
-
-// 获取所有Endpoint
-func (self *Etcd) FetchAll(upstreamID string) (endpoints []global.EndpointType, err error) {
-	return
-}
