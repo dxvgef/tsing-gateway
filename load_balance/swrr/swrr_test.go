4d645710c874c77e04871b98b6d759f6a4c3f826
dxvgef <dxvgef@outlook.com>
2020-06-04 20:10:09 +0800

    添加`Next()`方法的单元测试

---
+++ load_balance/swrr/swrr_test.go
@@ -0,0 +1,31 @@
+package swrr
+
+import (
+	"log"
+	"strconv"
+	"testing"
+)
+
+func TestNext(t *testing.T) {
+	var (
+		obj        InstType
+		upstreamID = "SWRR"
+		addr       string
+		weight     int
+	)
+	for i := 1; i <= 10; i++ {
+		if i == 6 {
+			addr = "10.0.0.6"
+			weight = 6
+		} else {
+			addr = "10.0.0." + strconv.Itoa(i)
+			weight = 1
+		}
+		obj.Set(upstreamID, addr, weight)
+		log.Println(addr, weight)
+	}
+
+	for i := 0; i < 10; i++ {
+		t.Log(obj.Next(upstreamID))
+	}
+}
