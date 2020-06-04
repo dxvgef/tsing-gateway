4d9043ccf8d2f9d9401b5222d20f42bbe4063045
dxvgef <dxvgef@outlook.com>
2020-06-04 20:10:47 +0800

    变更算法的结构，使用sync.Map替换map

--- load_balance/swrr/swrr.go
+++ load_balance/swrr/swrr.go
@@ -2,15 +2,16 @@ package swrr
 
 import (
 	"errors"
+	"sync"
+
+	"github.com/rs/zerolog/log"
 )
 
-var Pool *PoolType
+var Inst *InstType
+var globalNodes sync.Map // 节点列表 key=upstreamID, value=[]*NodeType
+var globalTotal sync.Map // 节点总量 key=upstreamID, value=int
 
-// Smooth Weighted Round-Robin 平滑加权轮循(与Nginx类似)
-type PoolType struct {
-	nodes     map[string][]*NodeType // 节点列表 map[upstreamID][addr]
-	nodeTotal map[string]int         // 节点总量 map[upstreamID]
-}
+type InstType struct{}
 
 // 节点结构
 type NodeType struct {
@@ -20,14 +21,12 @@ type NodeType struct {
 	EffectiveWeight int
 }
 
-func Init() *PoolType {
-	if Pool != nil {
-		return Pool
+func Init() *InstType {
+	if Inst != nil {
+		return Inst
 	}
-	var pool PoolType
-	pool.nodes = map[string][]*NodeType{}
-	pool.nodeTotal = map[string]int{}
-	return &pool
+	var inst InstType
+	return &inst
 }
 
 // // 降权
@@ -38,88 +37,110 @@ func Init() *PoolType {
 // 	}
 // }
 
-// 添加节点
-func (p *PoolType) Add(upstreamID, addr string, weight int) error {
-	if _, ok := p.nodes[upstreamID]; !ok {
-		p.nodes[upstreamID] = []*NodeType{}
-	}
-	for i := range p.nodes[upstreamID] {
-		if p.nodes[upstreamID][i].Addr == addr {
-			return errors.New("节点地址已存在")
-		}
-	}
-	node := NodeType{
-		EffectiveWeight: weight,
-		Weight:          weight,
-		Addr:            addr,
-	}
-	p.nodes[upstreamID] = append(p.nodes[upstreamID], &node)
-	p.nodeTotal[upstreamID]++
-	return nil
-}
-
 // 设置节点
-func (p *PoolType) Put(upstreamID, addr string, weight int) {
-	if _, ok := p.nodes[upstreamID]; !ok {
-		p.nodes[upstreamID] = []*NodeType{}
-	}
-	for i := range p.nodes[upstreamID] {
-		if p.nodes[upstreamID][i].Addr == addr {
-			p.nodes[upstreamID][i].Weight = weight
+func (self *InstType) Set(upstreamID, addr string, weight int) (err error) {
+	var (
+		nodes []*NodeType
+	)
+	mapValue, exist := globalNodes.Load(upstreamID)
+	if exist {
+		var ok bool
+		if nodes, ok = mapValue.([]*NodeType); !ok {
+			err = errors.New("类型断言失败")
+			log.Err(err).Caller().Msg("设置节点")
 			return
 		}
 	}
-	node := NodeType{
-		EffectiveWeight: weight,
-		Weight:          weight,
-		Addr:            addr,
+	for k := range nodes {
+		// 如果节点已存在，则直接更新
+		if nodes[k].Addr == addr {
+			nodes[k].Weight = weight
+			globalNodes.Store(upstreamID, nodes)
+			return self.Reset(upstreamID)
+		}
 	}
-	p.nodes[upstreamID] = append(p.nodes[upstreamID], &node)
-	p.nodeTotal[upstreamID]++
-}
 
-// 节点总数
-func (p *PoolType) Total(upstreamID string) int {
-	return p.nodeTotal[upstreamID]
+	// 插入节点
+	nodes = append(nodes, &NodeType{
+		Addr:   addr,
+		Weight: weight,
+	})
+	globalNodes.Store(upstreamID, nodes)
+	if err = self.Reset(upstreamID); err != nil {
+		return
+	}
+	// 递增节点总数
+	return self.updateTotal(upstreamID, 1)
 }
 
 // 移除指定地址的节点
-func (p *PoolType) Remove(upstreamID, addr string) {
-	for i := range p.nodes[upstreamID] {
-		if p.nodes[upstreamID][i].Addr == addr {
-			p.nodes[upstreamID] = append(p.nodes[upstreamID][:i], p.nodes[upstreamID][i+1:]...)
-			p.nodeTotal[upstreamID]--
-		}
+func (self *InstType) Remove(upstreamID, addr string) (err error) {
+	mapValue, exist := globalNodes.Load(upstreamID)
+	if !exist {
+		return nil
 	}
+	nodes, ok := mapValue.([]*NodeType)
+	if !ok {
+		err = errors.New("节点类型断言失败")
+		log.Err(err).Caller().Msg("移除节点")
+		return
+	}
+	for k := range nodes {
+		if nodes[k].Addr != addr {
+			continue
+		}
+		newNodes := append(nodes[:k], nodes[k+1:]...)
+		globalNodes.Store(upstreamID, newNodes)
+		if err = self.Reset(upstreamID); err != nil {
+			return
+		}
+		return self.updateTotal(upstreamID, -1)
+	}
+	return
+}
+
+// 节点总数
+func (self *InstType) Total(upstreamID string) int {
+	mapValue, exist := globalTotal.Load(upstreamID)
+	if !exist {
+		return 0
+	}
+	total, ok := mapValue.(int)
+	if !ok {
+		return 0
+	}
+	return total
 }
 
 // 选举出一个命中的节点
-func (p *PoolType) Next(upstreamID string) string {
-	if p.nodeTotal[upstreamID] == 0 {
+func (self *InstType) Next(upstreamID string) string {
+	nodes := self.getNodes(upstreamID)
+	if nodes == nil {
 		return ""
 	}
-	if p.nodeTotal[upstreamID] == 1 {
-		return p.nodes[upstreamID][0].Addr
+	nodeTotal := len(nodes)
+	if nodeTotal == 1 {
+		return nodes[0].Addr
 	}
 	var (
 		addr   string
 		target *NodeType
 	)
 	totalWeight := 0
-	for i := range p.nodes[upstreamID] {
-		if p.nodes[upstreamID][i] == nil {
-			continue
+	for i := range nodes {
+		nodes[i].CurrentWeight += nodes[i].EffectiveWeight
+		totalWeight += nodes[i].EffectiveWeight
+		if nodes[i].EffectiveWeight < nodes[i].Weight {
+			nodes[i].EffectiveWeight++
 		}
-		p.nodes[upstreamID][i].CurrentWeight += p.nodes[upstreamID][i].EffectiveWeight
-		totalWeight += p.nodes[upstreamID][i].EffectiveWeight
-		if p.nodes[upstreamID][i].EffectiveWeight < p.nodes[upstreamID][i].Weight {
-			p.nodes[upstreamID][i].EffectiveWeight++
-		}
-		if target == nil || p.nodes[upstreamID][i].CurrentWeight > target.CurrentWeight {
-			target = p.nodes[upstreamID][i]
-			addr = p.nodes[upstreamID][i].Addr
+		if target == nil || nodes[i].CurrentWeight > target.CurrentWeight {
+			target = nodes[i]
+			addr = nodes[i].Addr
 		}
 	}
+
+	globalNodes.Store(upstreamID, nodes)
+
 	if target == nil {
 		return ""
 	}
@@ -128,9 +149,75 @@ func (p *PoolType) Next(upstreamID string) string {
 }
 
 // 重置所有节点的状态
-func (p *PoolType) Reset(upstreamID string) {
-	for i := range p.nodes[upstreamID] {
-		p.nodes[upstreamID][i].EffectiveWeight = p.nodes[upstreamID][i].Weight
-		p.nodes[upstreamID][i].CurrentWeight = 0
+func (self *InstType) Reset(upstreamID string) (err error) {
+	mapValue, exist := globalNodes.Load(upstreamID)
+	if !exist {
+		return nil
 	}
+	nodes, ok := mapValue.([]*NodeType)
+	if !ok {
+		err = errors.New("节点类型断言失败")
+		log.Err(err).Caller().Msg("重置节点")
+		return
+	}
+	for k := range nodes {
+		nodes[k].EffectiveWeight = nodes[k].Weight
+		nodes[k].CurrentWeight = 0
+	}
+	return nil
+}
+
+// 更新节点统计总数
+func (self *InstType) updateTotal(upstreamID string, count int) (err error) {
+	mapValue, exist := globalTotal.Load(upstreamID)
+	if !exist {
+		globalTotal.Store(upstreamID, 0)
+		return nil
+	}
+
+	total, ok := mapValue.(int)
+	if !ok {
+		err = errors.New("节点类型断言失败")
+		log.Err(err).Caller().Msg("更新节点总数")
+		return err
+	}
+	total += count
+	globalTotal.Store(upstreamID, total)
+	return nil
+}
+
+// 判断节点是否存在
+func (self *InstType) nodeExist(upstreamID, addr string) (exist bool) {
+	if _, exist = globalNodes.Load(upstreamID); !exist {
+		return
+	}
+	globalNodes.Range(func(key, value interface{}) bool {
+		if key.(string) == upstreamID {
+			nodes, ok := value.([]*NodeType)
+			if !ok {
+				return true
+			}
+			for k := range nodes {
+				if nodes[k].Addr == addr {
+					exist = true
+					return false
+				}
+			}
+		}
+		return true
+	})
+	return
+}
+
+// 获得节点
+func (self *InstType) getNodes(upstreamID string) []*NodeType {
+	mapValue, exist := globalNodes.Load(upstreamID)
+	if !exist {
+		return nil
+	}
+	nodes, ok := mapValue.([]*NodeType)
+	if !ok {
+		return nil
+	}
+	return nodes
 }
