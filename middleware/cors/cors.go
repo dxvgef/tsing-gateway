277ffc9afef0e5a527c879b9a52b4e9d51fab1d5
dxvgef <dxvgef@outlook.com>
2020-07-09 19:25:35 +0800

    修复cors不能放行非get/post请求的问题

--- middleware/cors/cors.go
+++ middleware/cors/cors.go
@@ -2,7 +2,7 @@ package cors
 
 import (
 	"net/http"
-	"strings"
+	"strconv"
 
 	"github.com/rs/zerolog/log"
 
@@ -11,18 +11,21 @@ import (
 
 // CORS
 type CORS struct {
-	AllowOrigins     []string `json:"allow_origins,omitempty"`     // 允许客户端的来源域
-	ExposeHeaders    []string `json:"expose_headers,omitempty"`    // 允许响应的头信息
-	AllowCredentials bool     `json:"allow_credentials,omitempty"` // 允许客户端携带cookie
-	AllowMethods     []string `json:"allow_methods,omitempty"`     // 允许客户端请求的方法
-	AllowHeaders     []string `json:"allow_headers,omitempty"`     // 允许客户端请求的头信息
+	AllowOrigins     string `json:"allow_origins,omitempty"`     // 允许客户端的来源域
+	ExposeHeaders    string `json:"expose_headers,omitempty"`    // 允许响应的头信息
+	AllowCredentials bool   `json:"allow_credentials,omitempty"` // 允许客户端携带cookie
+	AllowMethods     string `json:"allow_methods,omitempty"`     // 允许客户端请求的方法
+	AllowHeaders     string `json:"allow_headers,omitempty"`     // 允许客户端请求的头信息
 }
 
 // 新建中间件实例
 func New(config string) (*CORS, error) {
 	var instance CORS
-	instance.AllowOrigins = []string{"*"}
-	instance.AllowMethods = []string{"*"}
+	instance.AllowOrigins = "*"
+	instance.AllowMethods = "GET,POST,PUT,DELETE,OPTIONS,PATCH"
+	instance.AllowHeaders = "Access-Control-Allow-Headers,token,Origin,X-Requested-With,Content-Type,Accept,X-Token"
+	instance.AllowCredentials = true
+	instance.ExposeHeaders = "*"
 	err := instance.UnmarshalJSON(global.StrToBytes(config))
 	if err != nil {
 		log.Err(err).Caller().Send()
@@ -37,44 +40,14 @@ func (self *CORS) GetName() string {
 
 // 中间件行为
 func (self *CORS) Action(resp http.ResponseWriter, req *http.Request) (bool, error) {
-	if len(self.AllowOrigins) > 0 {
-		if self.AllowMethods[0] == "*" {
-			resp.Header().Set("Access-Control-Allow-Origin", "*")
-		} else {
-			resp.Header().Set("Access-Control-Allow-Origin", strings.Join(self.AllowOrigins, ","))
-		}
+	resp.Header().Set("Access-Control-Allow-Origin", self.AllowOrigins)
+	resp.Header().Set("Access-Control-Allow-Methods", self.AllowMethods)
+	resp.Header().Set("Access-Control-Allow-Headers", self.AllowHeaders)
+	resp.Header().Set("Access-Control-Expose-Headers", self.ExposeHeaders)
+	resp.Header().Set("Access-Control-Allow-Credentials", strconv.FormatBool(self.AllowCredentials))
+	if req.Method == "OPTIONS" {
+		resp.WriteHeader(http.StatusNoContent)
+		return true, nil
 	}
-	if len(self.AllowMethods) > 0 {
-		if self.AllowMethods[0] == "*" {
-			resp.Header().Set("Access-Control-Allow-Methods", "*")
-		} else {
-			resp.Header().Set("Access-Control-Allow-Methods", strings.Join(self.AllowMethods, ","))
-		}
-	}
-	if len(self.AllowHeaders) > 0 {
-		if self.AllowHeaders[0] == "*" {
-			resp.Header().Set("Access-Control-Allow-Headers", "*")
-		} else {
-			resp.Header().Set("Access-Control-Allow-Headers", strings.Join(self.AllowHeaders, ","))
-		}
-	}
-	if len(self.ExposeHeaders) > 0 {
-		if self.ExposeHeaders[0] == "*" {
-			resp.Header().Set("Access-Control-Expose-Headers", "*")
-		} else {
-			resp.Header().Set("Access-Control-Expose-Headers", strings.Join(self.ExposeHeaders, ","))
-		}
-	}
-	if len(self.ExposeHeaders) > 0 {
-		if self.ExposeHeaders[0] == "*" {
-			resp.Header().Set("Access-Control-Expose-Headers", "*")
-		} else {
-			resp.Header().Set("Access-Control-Expose-Headers", strings.Join(self.ExposeHeaders, ","))
-		}
-	}
-	if self.AllowCredentials {
-		resp.Header().Set("Access-Control-Allow-Credentials", "true")
-	}
-
 	return false, nil
 }
