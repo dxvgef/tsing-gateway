c12abd5ad8174948b5ec9aa7435c755274de4b57
dxvgef <dxvgef@outlook.com>
2020-07-27 18:37:07 +0800

    修复多级路径的通配符路由的bug

--- proxy/route.go
+++ proxy/route.go
@@ -3,10 +3,11 @@ package proxy
 import (
 	"errors"
 	"net/http"
-	"path"
 	"strings"
 
 	"local/global"
+
+	"github.com/rs/zerolog/log"
 )
 
 // 设置路由组及路由
@@ -58,119 +59,127 @@ func DeleteRoute(routeGroupID, routePath, routeMethod string) error {
 // 匹配路由，返回集群ID和匹配结果的HTTP状态码
 func matchRoute(req *http.Request) (hostname string, service global.ServiceType, status int) {
 	var (
-		routeGroupID string
-		routePath    = req.URL.Path
-		routeMethod  = req.Method
-		matchResult  bool
-		serviceID    string
+		routeGroupID, serviceID string
+		routePath               = req.URL.Path
+		routeMethod             = req.Method
+		matchResult, exist      bool
+		key                     strings.Builder
 	)
+	if routePath == "" {
+		routePath = "/"
+	}
 
-	// 匹配主机
+	// -------------------------------------- 匹配主机 -----------------------------------------------
 	hostname, routeGroupID, matchResult = matchHost(req.Host)
 	if !matchResult {
 		status = http.StatusServiceUnavailable
 		return
 	}
 
-	if routePath == "" {
-		routePath = "/"
-	}
-	var key strings.Builder
-	var exist bool
-	// 先尝试直接匹配路由
+	// -------------------------------------- 匹配服务 -----------------------------------------------
+	// 尝试精确匹配路径和方法
 	key.WriteString(routeGroupID)
 	key.WriteString("/")
 	key.WriteString(routePath)
 	key.WriteString("/")
 	key.WriteString(routeMethod)
-	if v, e := global.Routes.Load(key.String()); e {
-		service, exist = matchService(v.(string))
+	if k, v := global.Routes.Load(key.String()); v {
+		service, exist = matchService(k.(string))
 		if exist {
 			return
 		}
 	}
 
-	// --------------- 以下用于路径和方法的模糊匹配，并且判断要返回哪种http状态码 -----------------------
-	// 先尝试匹配精确路径
+	// 尝试只精确匹配路径
+	key.Reset()
 	key.WriteString(routeGroupID)
 	key.WriteString("/")
 	key.WriteString(routePath)
 	key.WriteString("/")
-	paths := map[string]string{}
-	global.Routes.Range(func(k, v interface{}) bool {
-		p := k.(string)
-		if strings.HasPrefix(p, key.String()) {
-			paths[p] = v.(string)
-		}
-		return true
-	})
-	// 如果没有匹配到精确路径，开始尝试匹配模糊路径
-	if len(paths) == 0 {
-		pathLastChar := routePath[len(routePath)-1]
-		// 如果路径的最后一个字符是/
-		if pathLastChar != 47 {
-			pos := strings.LastIndex(routePath, path.Base(routePath))
-			routePath = routePath[:pos]
-		}
-		routePath = routePath + "*"
-		key.Reset()
-		key.WriteString(routeGroupID)
-		key.WriteString("/")
-		key.WriteString(routePath)
-		key.WriteString("/")
-		// 尝试模糊匹配
-		global.Routes.Range(func(k, v interface{}) bool {
-			p := k.(string)
-			if strings.HasPrefix(p, key.String()) {
-				paths[p] = v.(string)
-			}
-			return true
-		})
+	var (
+		reqGroupID, reqPath, reqMethod string
+		err                            error
+	)
+	// 从请求中解析出信息
+	reqGroupID, reqPath, reqMethod, err = global.ParseRouteFromKey(key.String()+routeMethod, "")
+	if err != nil {
+		status = http.StatusInternalServerError
+		// 此处error会由客户端请求触发，因此不记录日志
+		return
 	}
-	// 如果精确和模糊路径都没匹配到，返回404错误
-	if len(paths) == 0 {
-		status = http.StatusNotFound
+	// 根据精准路径匹配服务
+	serviceID, status, err = matchPath(false, reqGroupID, reqPath, reqMethod)
+	if err != nil {
+		log.Err(err)
+	}
+	if serviceID == "" {
+		// 根据通配路径匹配服务
+		serviceID, status, err = matchPath(true, reqGroupID, reqPath, reqMethod)
+	}
+	if err != nil {
+		status = http.StatusInternalServerError
+		// 此处error会由客户端请求触发，因此不记录日志
+		return
+	}
+	if status != 0 {
 		return
 	}
 
-	// 尝试匹配精确方法
-	for k, v := range paths {
-		key.Reset()
-		key.WriteString(routeGroupID)
-		key.WriteString("/@")
-		key.WriteString(routePath)
-		key.WriteString("/")
-		key.WriteString(routeMethod)
-		if strings.HasPrefix(k, key.String()) {
-			serviceID = v
-			break
-		}
-	}
-	// 尝试匹配ANY方法
-	if serviceID == "" {
-		for k, v := range paths {
-			key.Reset()
-			key.WriteString(routeGroupID)
-			key.WriteString("/")
-			key.WriteString(routePath)
-			key.WriteString("/ANY")
-			if strings.HasPrefix(k, key.String()) {
-				serviceID = v
-				break
-			}
-		}
-	}
-	if serviceID == "" {
-		status = http.StatusMethodNotAllowed
-		return
-	}
-
-	// 获得服务
+	// 匹配服务
 	exist = false
 	service, exist = matchService(serviceID)
 	if !exist {
 		status = http.StatusNotImplemented
 		return
 	}
+	return
+}
+
+// 匹配路径
+func matchPath(isWildcard bool, reqGroupID, reqPath, reqMethod string) (serviceID string, status int, err error) {
+	var (
+		keyGroupID, keyPath, keyMethod string
+	)
+	global.Routes.Range(func(k, v interface{}) bool {
+		keyStr := k.(string)
+		// 从key中解析出信息
+		keyGroupID, keyPath, keyMethod, err = global.ParseRouteFromKey(keyStr, "")
+		if err != nil {
+			log.Err(err).Caller().Send()
+			return false
+		}
+		// 对比路由组
+		if keyGroupID != reqGroupID {
+			return true
+		}
+
+		// 匹配精准路径
+		if !isWildcard && reqPath == keyPath {
+			if keyMethod == "ANY" {
+				serviceID = v.(string)
+				return false
+			}
+			if keyMethod == reqMethod {
+				serviceID = v.(string)
+				return false
+			}
+			status = http.StatusMethodNotAllowed
+			return false
+		}
+		// 匹配通配路径
+		if isWildcard && strings.HasPrefix(reqPath, keyPath[:len(keyPath)-1]) {
+			if keyMethod == "ANY" {
+				serviceID = v.(string)
+				return false
+			}
+			if keyMethod == reqMethod {
+				serviceID = v.(string)
+				return false
+			}
+			status = http.StatusMethodNotAllowed
+			return false
+		}
+		return true
+	})
 	return
 }
